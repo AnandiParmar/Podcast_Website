92f49fced6410f4a2c2bbd7481e61820d9eab32c
Krinal111 <krinal.trueline01@gmail.com>
2024-08-05 21:09:06 +0530

    fixed Navbar

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -28,9 +28,7 @@ const Footer =() =>
             <a href="" className="me-4 text-reset">
                 <i className="fab fa-linkedin"></i>
             </a>
-            <a href="" className="me-4 text-reset">
-                <i className="fab fa-github"></i>
-            </a>
+            
         </div>
     
     </section>
