c3ae101618f53714444de9ab5c2cdde9b569935a
AnandiParmar <ananditrueline99@gmail.com>
2024-09-01 17:54:04 +0530

    logo added

--- src/common/navbar/Navbar.jsx
+++ src/common/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import {Link,NavLink, useNavigate} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './Navbar.css'
-
+import podcast_logo from "../../images/podcast_logo.jpg"
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(true);
@@ -18,22 +18,23 @@ function gotosignin(){
   navigate('/signin')
 }
   return (
-  <nav className='d-flex navbar fixed-top' >
+  <nav className='d-flex navbar fixed-top ' >
 
-    <a href='#' className='text-white'>Navbar</a>
+    <img src={podcast_logo} alt="" id='logo'/>
     <div className='container'>
         <ul className={isOpen==true?'#navbar active mx-auto animate-slideInRight':'#navbar mx-auto'} id="navbar">
-            <li><Link to="/">Home</Link></li>
+            <div className='d-flex'><li><Link to="/">Home</Link></li>
             <li><Link to="/about">About us</Link></li>
             <li><Link to="/podcast">Podcast</Link></li>
             <li><Link to="/contactus">Contact Us</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            
-        </ul>
-    </div>
-    <div>
+            <li><Link to="/profile">Profile</Link></li></div>
+            <div>
       <button className='btn btn-secondary text-right mr-10' onClick={gotosignin}>Sign In</button>
+    </div>     
+        </ul>
+       
     </div>
+    
 
     <div id="mobile">
         <i id="bar" onClick={handleClick} className={isOpen==true?'fas fa-bars ':'fas fa-times'}></i>
