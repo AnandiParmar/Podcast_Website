cd2d852655353065094943f1897335e5e9562e5a
AnandiParmar <ananditrueline99@gmail.com>
2024-09-30 18:05:02 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import B from "./components/B/B";
 import Home from "./components/Home/Home";
 import Latest from "./components/Latest/Latest";
 import Recent from "./components/Recent/Recent";
-import './App.css'
+
 import Footer from './common/footer/Footer'
 import Register from './common/LoginRegister/Register'
 import Navbar from './common/navbar/Navbar'
-// import About from './pages/About'
-import Home from './pages/HomePage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Podcast from './pages/Podcast'
 import Signin from './pages/Signin'
@@ -37,7 +35,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact Component={Home}></Route>
-          {/* <Route path="/about" exact Component={About}></Route> */}
           <Route path='/podcast' exact Component={Podcast}></Route>
           <Route path='/profile' exact Component={profile}></Route>
           <Route path='/signin' exact Component={Signin}></Route>
